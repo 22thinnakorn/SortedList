bd85f4d2ff752cb025ebc6aa880ad35f87e2bd7b
22thinnakorn <22thinnakorn@gmail.com>
2021-10-19 04:24:23 +0700

    SortedList

---
+++ Lec86SortedList/Lec86SortedList/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lec86SortedList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SortedList sortedList;
+        public MainWindow()
+        {
+            sortedList = new SortedList();
+            InitializeComponent();
+        }
+
+        private void showtbn_Click(object sender, RoutedEventArgs e)
+        {
+            string data1 = "";
+            sortedList.Add(keytxt.Text, valuetxt.Text);
+            ICollection collection = sortedList.Keys;
+            foreach(string key in collection)
+            {
+               data1 = data1 + sortedList[key].ToString()+"\n";
+            }
+            MessageBox.Show(data1);
+                   
+        }
+    }
+}
